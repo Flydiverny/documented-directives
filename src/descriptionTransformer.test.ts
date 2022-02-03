8503782d9e63e2a363ba97ed3aafe1aafb171323
jrasmusbm <JRasmusBm@gmail.com>
2022-02-03 20:38:38 +0100

    chore(test): Add a test for a custom exposedFilter

--- src/descriptionTransformer.test.ts
+++ src/descriptionTransformer.test.ts
@@ -3,7 +3,8 @@ import { GraphQLSchema, printSchema } from 'graphql'
 import { descriptionTransformer } from './descriptionTransformer'
 import prettier from 'prettier'
 
-const format = (string: string) => prettier.format(string, { semi: false, parser: 'graphql' })
+const format = (string: string) =>
+  prettier.format(string, { semi: false, parser: 'graphql' })
 
 const expectSchema = (schema: GraphQLSchema) => {
   return {
@@ -51,6 +52,61 @@ describe('descriptionTransformer', () => {
         foo bar is cool
 
         @deprecated(reason: no more foos to give)
+        """
+        fooBar: String @deprecated(reason: "no more foos to give")
+      }
+    `)
+  })
+
+  it('Supports a custom exposedFilter', async (): Promise<void> => {
+    let schema = makeExecutableSchema({
+      typeDefs: [
+        /* GraphQL */ `
+          enum Role {
+            ADMIN
+          }
+
+          directive @requireAuth(role: Role) on FIELD_DEFINITION
+
+          type Query {
+            "Run Hello World"
+            helloWorld: String @deprecated
+            "Cow say MOOO"
+            cowSay: String @requireAuth(role: ADMIN)
+            "foo bar is cool"
+            fooBar: String @deprecated(reason: "no more foos to give")
+          }
+        `,
+      ],
+      resolvers: {},
+    })
+
+    schema = descriptionTransformer(schema, {
+      exposedFilter: (directive) => ['requireAuth'].includes(directive.name),
+    })
+
+    expectSchema(schema).toMatch(/* GraphQL */ `
+      directive @requireAuth(role: Role) on FIELD_DEFINITION
+
+      enum Role {
+        ADMIN
+      }
+
+      type Query {
+        """
+        Run Hello World
+        """
+        helloWorld: String @deprecated
+
+        """
+        Cow say MOOO
+
+        @requireAuth(role: ADMIN)
+        """
+        cowSay: String
+
+        """
+        foo bar is cool
         """
         fooBar: String @deprecated(reason: "no more foos to give")
       }
