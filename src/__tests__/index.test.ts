9236057069f95833012529f4267a64673569b659
jrasmusbm <JRasmusBm@gmail.com>
2022-02-03 20:38:38 +0100

    test: Add a test for a custom exposedFilter

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,18 +1,12 @@
 import { makeExecutableSchema } from "@graphql-tools/schema";
-import {
-  GraphQLSchema,
-  Kind,
-  ObjectTypeDefinitionNode,
-  parse,
-  printSchema,
-} from "graphql";
+import { printSchema } from "graphql";
 import { descriptionTransformer } from "..";
 
 describe("descriptionTransformer", () => {
   it("Adds directives as comments", async (): Promise<void> => {
     let schema = makeExecutableSchema({
       typeDefs: [
-        /* GraphQL */ `
+        `
           type Query {
             "Run Hello World"
             helloWorld: String @deprecated
@@ -28,7 +22,8 @@ describe("descriptionTransformer", () => {
 
     schema = descriptionTransformer(schema);
 
-    expect(printSchema(schema)).toEqual(`
+    expect(printSchema(schema)).toEqual(
+      `
 type Query {
   """
   Run Hello World
@@ -47,6 +42,60 @@ type Query {
   """
   fooBar: String @deprecated(reason: "no more foos to give")
 }
-    `.trim());
+    `.trim()
+    );
+  });
+
+  it("Supports a custom exposedFilter", async (): Promise<void> => {
+    let schema = makeExecutableSchema({
+      typeDefs: [
+        `
+          enum Role {
+            ADMIN
+          }
+
+          directive @requireAuth(role: Role) on FIELD_DEFINITION
+
+          type Query {
+            "Run Hello World"
+            helloWorld: String @deprecated
+            "Cow say MOOO"
+            cowSay: String @requireAuth(role: ADMIN)
+            "foo bar is cool"
+            fooBar: String @deprecated(reason: "no more foos to give")
+          }
+        `,
+      ],
+      resolvers: {},
+    });
+
+    schema = descriptionTransformer(schema, {
+      exposedFilter: (directive) => ["requireAuth"].includes(directive.name),
+    });
+
+    expect(printSchema(schema)).toEqual(
+      `
+directive @requireAuth(role: Role) on FIELD_DEFINITION
+
+enum Role {
+  ADMIN
+}
+
+type Query {
+  """Run Hello World"""
+  helloWorld: String @deprecated
+
+  """
+  Cow say MOOO
+  
+  @requireAuth(role: ADMIN)
+  """
+  cowSay: String
+
+  """foo bar is cool"""
+  fooBar: String @deprecated(reason: "no more foos to give")
+}
+    `.trim()
+    );
   });
 });
